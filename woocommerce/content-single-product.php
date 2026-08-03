0f36057ef76045402eeabc759183a435a1c96a2d
Sozib Alahi <sozibalahi@gmail.com>
2026-08-03 17:46:12 +0600

    Fix image zoom hover transformOrigin coordinates template string

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -2211,7 +2211,7 @@ if ( woocom_is_grocery_product( $main_product_id ) ) {
                             const rect = slide.getBoundingClientRect();
                             const x = ((e.clientX - rect.left) / rect.width) * 100;
                             const y = ((e.clientY - rect.top) / rect.height) * 100;
-                            img.style.transformOrigin = `${x}% 	ext{${y}%}`;
+                            img.style.transformOrigin = `${x}% ${y}%`;
                             img.style.transform = 'scale(2.3)';
                         }
                     });
